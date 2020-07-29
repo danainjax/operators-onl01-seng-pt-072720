2d5937f1b4007e07dfd956bc1b8373d3a85630d6
Dana Pughakoff <danainjax@gmail.com>
2020-07-29 10:57:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+if speed < 40
+  true
+  elsif speed > 60
+  true
+else
+  false
 end
 
 
-
-def not_safe?(speed)
-	
+def_not_safe?(speed)
+speed < 40 || speed> 60 ? true : false 
 end
-	
 
 
